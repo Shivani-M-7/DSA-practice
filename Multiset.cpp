49f71373780b78629b42c0e24fc99ae58d736688
Shivani-M-7 <99704125+Shivani-M-7@users.noreply.github.com>
2022-07-15 17:35:45 +0530

    Create Multiset.cpp

---
+++ Multiset.cpp
@@ -0,0 +1,31 @@
+//https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/monk-and-the-magical-candy-bags/
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+	//cout << "Input number is " << num << endl;	// Writing output to STDOUT
+	
+	while(num--)
+	{
+		long long candies,n,k;
+		cin>>n>>k;
+		multiset<long long>bags;
+		for(int i=0;i<n;i++)
+		{
+		cin>>candies;
+		bags.insert(candies);
+		}
+		long long total=0;
+		for(int i=0;i<k;i++)
+		{
+			auto it=--bags.end();
+			total+=(*it);
+			long long k=(*it)/2;
+			bags.erase(it);
+			bags.insert(k);
+		}
+		cout<<total<<endl;
+	} 
+}
